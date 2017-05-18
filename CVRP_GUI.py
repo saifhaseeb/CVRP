1d101f5bd3be0f44f076787c2789582d0572176c
saifhaseeb <smhas12@student.monash.edu>
2017-05-18 19:30:01 +1000

    content = content of vrp file, line by line

--- CVRP_GUI.py
+++ CVRP_GUI.py
@@ -4,31 +4,28 @@ from tkinter.filedialog import askopenfilename
 from tkinter import messagebox
 import os.path
 
+def getNum(x):
+    return int(''.join(ele for ele in x if ele.isdigit() or ele == '.'))
 
-def openfile():
+def openFile():
 
     filename = askopenfilename(parent=root)
     extension = os.path.splitext(filename)[1][1:]
 
     if extension == '':
-        messagebox.showinfo("Error", "Wrong file input, please choose a vrp file")
+        messagebox.showinfo("Error", "No file chosen, please click on a valid test file")
 
     else:
         with open(filename) as f:
             content = f.readlines()
 
         content = [x.strip() for x in content]
-        print(content)
+        totalnode = getNum(content[3])
+
 
 
 
 root = Tk()
-menubar = Menu(root)
-filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="Open", command=openfile)
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=root.quit)
-menubar.add_cascade(label="File", menu=filemenu)
-
-root.config(menu=menubar)
+b = Button(root, text="Open VRP File", command=openFile)
+b.pack()
 root.mainloop()
